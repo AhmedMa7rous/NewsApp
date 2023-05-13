62634f5e8136f3e7c22568246bbf99b29c84f370
Ma7rous <ahmedmahros425@gmail.com>
2023-05-13 18:59:01 +0300

    [NEW-11] Offline mode Created

---
+++ NewsApp/Network/ConnectionTypes.swift
@@ -0,0 +1,15 @@
+//
+//  ConnectionTypes.swift
+//  NewsApp
+//
+//  Created by Adam on 13/05/2023.
+//
+
+import Foundation
+
+enum ConnectionType {
+    case wifi
+    case cellular
+    case ethernet
+    case unknown
+}
